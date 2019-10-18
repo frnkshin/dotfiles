17f76c82daac8274110a865bc8a2f25b84746110
frank shin <im@frnkshin.io>
2019-10-17 21:17:58 -0400

    removed trash

--- emacs.d/elpa/monitor-20161018.444/monitor-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; monitor-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("monitor-pkg.el" "monitor.el") (23055
-;;;;;;  44984 390112 589000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; monitor-autoloads.el ends here
--- emacs.d/elpa/org-wc-20170727.1911/org-wc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-wc" "20170727.1911" "Count words in org mode trees." 'nil :commit "d294ad7117c150445e6166fc0d88c14a8386f34e")
